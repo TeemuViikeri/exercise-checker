9bbbdc20d3cd3664fbc697e8070ca9250f30ccc1
Teemu Viikeri <teemu.viikeri@tuni.fi>
2021-11-01 13:22:19 +0200

    Add working checker version.

--- index.js
+++ index.js
@@ -1,80 +1,138 @@
 const schedule = require('node-schedule')
 const { exec, execSync } = require('child_process')
-const fs = require('fs');
-const { stdout, stderr } = require('process');
+const fs = require('fs')
+const path = require('path')
 
 try {
-	console.log("Scheduling...");
-	schedule.scheduleJob('*/10 * * * * *', () => {
-		console.log("Checking...");
+  console.log('Scheduling...')
+  // Schedule function call every 10 seconds
+  schedule.scheduleJob('*/30 * * * * *', () => {
+    // Find all directiories that start with tamk-java-* and put the output into text file
+    execSync('find . -name tamk-java-* > dirs.txt')
 
-		execSync('find . -name tamk-java-* > dirs.txt')
-		const dData = fs.readFileSync('dirs.txt', 'utf8')
-		const dirs = dData.split("\n")
-		dirs.pop()
-		console.log(dirs);
+    // Put the directory list into a string variable
+    const dData = fs.readFileSync('dirs.txt', 'utf8')
+    const dirs = dData.split('\n')
+    dirs.pop()
 
-		dirs.forEach((dir) => {
-			try {
-				execSync('rm ./reports/report.txt', { cwd: dir})
-			} catch (error) {
-				console.log("report.txt not found");	
-			}
+    console.log(dirs)
 
-			exec('find -name *.java > files.txt', { cwd: dir }, (error, stdout, stderr) => {
-				if (error) {
-					console.log("Error in finding Java files and creating output file");
-					return
-				}
+    dirs.forEach((dir) => {
+      console.log('--- Directories ---')
+      console.log(`dir: ${dir}`)
 
-				const fData = fs.readFileSync(`${dir}/files.txt`, 'utf8')
-				console.log(fData);
-				const files = fData.split("\n")
-				files.pop()
-				
-				files.forEach((file) => {
-					execSync(`pwd | javac ${file}`, { cwd: dir }, (error, stdout, stderr) => {
-						const fName = file.substring(file.lastIndexOf("/"), 3)
-						console.log(fName);
-						fs.appendFileSync('./reports/report.txt',`${fName}: ${error}`, 'utf8')
+      console.log();
 
-						// if (error) {
-						// 	console.log(`Error in compiling file ${fName}`);
-						// 	return
-						// }
-					})
-				})
-			})
-  
-			// exec(`javac `, (error, stdout, stderr) => {
-			// 	execSync('touch ./reports/report.txt')
+      // Create regex for directory name
+      const dRegex = /tamk-java-\w+-\w+/
+      // Search for the file name from the file path
+      const dName = dir.match(dRegex)[0]
+      console.log(`dName: ${dName}`)
 
-			// 	if (error) {
-			// 		console.error(`javac exec error: ${error}`);
-			// 		fs.appendFileSync('E01: \n')
-			// 	}
-			// })
-		})
- 
-		// exec('javac *.java', (error, stdout, stderr) => {
-		// 	if (error) {
-		// 		console.error(`javac exec error: ${error}`);
-		// 		return;
-		// 	}
+      // Create a new path object for report.txt
+      const reports = path.join(__dirname, 'drive', dName, 'reports')
+      const rPath = path.join(reports, 'report.txt')
+      console.log(`rPath: ${rPath}`)
 
-		// 	exec('java e01', (error, stdout, stderr) => {
-		// 		if (error) {
-		// 			console.error(`java exec error: ${error}`);
-		// 			return;
-		// 		}	
-		// 			console.log(`java stdout: ${stdout}`);
-		// 			console.error(`java stderr: ${stderr}`);
-		// 		})
-		// 	});
+      try {
+        console.log('Creating report file...')
+        // Remove report text file under the current directory
+        fs.writeFileSync(rPath, 'REPORT\n')
+      } catch (error) {
+        console.log('There was an issue with creating report.txt')
+        console.log(error)
+      }
 
-		// 	console.log(`javac stdout: ${stdout}`);
-		// 	console.error(`javac stderr: ${stderr}`);
-	})
+      try {
+        // Find all java files under current directory and add them to text file
+        execSync('find -name *.java > files.txt', { cwd: dir })
+      } catch (error) {
+        console.log('Error in finding Java files and creating output file')
+        return
+      }
+
+      let fData = ''
+
+      // Put the file list into a string variable
+      fData = fs.readFileSync(`${dir}/files.txt`, 'utf8')
+      const files = fData.split('\n')
+      files.pop()
+
+      files.forEach((file) => {
+        console.log('--- Files ---')
+        console.log(`dir: ${dir}`)
+        console.log(`file: ${file}`)
+
+        // Create regex for file name
+        const fRegex = /e\d{2}/
+        // Search for the file name from the file path
+        const fName = file.match(fRegex)[0]
+        console.log(`fName: ${fName}`)
+
+        try {
+          console.log('--- javac ---')
+          console.log('compiling...')
+          // Create a path object for the file
+          const fPath = path.join(dir, 'exercises', fName)
+          console.log(`fPath: ${fPath}`)
+          // Compile the current file
+          execSync(`javac ${fName}.java`, { cwd: fPath })
+          console.log('compiled...')
+        } catch (error) {
+          console.log(rPath)
+          fs.appendFileSync(rPath, `${fName}: ${error}\n`, 'utf8')
+          return
+        }
+
+        let output = ''
+
+        try {
+          console.log('--- java ---')
+          console.log('running...')
+          output = execSync(`java ${fName}`, {
+            cwd: `${dir}/exercises/${fName}`,
+          })
+            .toString()
+            .trim()
+        } catch (error) {
+          fs.appendFileSync(rPath, `${fName}: ${error}\n`, 'utf8')
+          return
+        }
+
+        check(fName, output, dName)
+      })
+    })
+  })
 } catch (error) {
-	
+  console.log('There was an error in scheduling...')
+  console.log(error)
+}
+
+const check = (exercise, answer, dName) => {
+  console.log('checking...')
+
+  const reports = path.join(__dirname, 'drive', dName, 'reports')
+  const rPath = path.join(reports, 'report.txt')
+  console.log(`rPath: ${rPath}`)
+
+  console.log('reporting...')
+  switch (exercise) {
+    case 'e01':
+      if (answer === 'Hello World') {
+        fs.appendFileSync(rPath, `e01: OK\n`)
+      } else {
+        fs.appendFileSync(rPath, `e01: WRONG\n`)
+      }
+      break
+    case 'e02':
+      if (answer === '4') {
+        fs.appendFileSync(rPath, `e02: OK\n`)
+      } else {
+        fs.appendFileSync(rPath, `e02: WRONG\n`)
+      }
+      break
+    default:
+      break
+  }
+  console.log()
 }
